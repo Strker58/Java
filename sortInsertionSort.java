99e5a0f2cf98829730bf7a50e44de3f6b3d55701
Strker58 <58742932+Strker58@users.noreply.github.com>
2021-09-15 17:40:34 +0530

    Insertion sort. REAL ONE!!

--- sortInsertionSort.java
+++ sortInsertionSort.java
@@ -3,24 +3,23 @@ import java.util.*;
 class sortInsertionSort
 {
   public static void main(String[] args)
-  {
-      int[] a={1,4,7,2,5,1,6,8,34,0,23,122,3,4,5};
-      int n=a.length;int tmp;
-      for(int i=0;i<n;i++)
-      {
-        for(int j=i;j<n;j++)
-        {
-          if(a[i]>a[j])
-          {
-            tmp=a[i];
-            a[i]=a[j];
-            a[j]=tmp;
-          }
-        }
-      }
-      for(int i=0;i<n;i++)
-      {
-        System.out.print(a[i]+" ");
-      }
-  }
+ {
+     int[] a={1,4,7,2,5,1,6,8,34,0,23,122,3,4,5};
+     int n=a.length;int tmp;
+     for(int i=1;i<n;i++)
+     {
+       int j=i-1;
+       tmp=a[i];
+       while(j>=0 && a[j]>tmp)
+       {
+         a[j+1]=a[j];
+         j--;
+       }
+       a[j+1]=tmp;
+     }
+     for(int i=0;i<n;i++)
+     {
+       System.out.print(a[i]+" ");
+     }
+ }
 }
